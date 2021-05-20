1476401607b0a9ab9cbff1cd6098c2d10317a730
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-05-20 10:55:02 +0900

    Update get.py

--- get.py
+++ get.py
@@ -11,24 +11,22 @@ scd30.start_periodic_measurement()
 
 time.sleep(2)
 
-t0 = time.time()
+
 mqtt_client = mqtt.Client()
 mqtt_client.connect("fluent-bit",1883, 60)
 while True:
     if scd30.get_data_ready():
-        if (time.time() - t0)>10 :
-            m = scd30.read_measurement()
-            if m is not None:
-                tim = '"timestamp":"'+datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S.%f')+'"'
-                temp = '"' + "temp(degree)" + '"' + ":" + '"' + str(round(m[1],3)) + '"'
-                hum = '"' + "humid(%)" + '"' + ":" + '"' + str(round(m[2],3)) + '"'
-                co2 = '"' + "humid(%)" + '"' + ":" + '"' + str(round(m[0],3)) + '"'
-                mylist = [tim,temp,hum,co2]
-                mystr = '{' + ','.join(map(str,mylist))+'}'
-                print(mystr)
-                mqtt_client.publish("{}/{}".format("/demo",'car_count'), mystr)
-                time.sleep(2)
-            t0 = time.time()
+        m = scd30.read_measurement()
+        if m is not None:
+            tim = '"timestamp":"'+datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S.%f')+'"'
+            temp = '"' + "temp(degree)" + '"' + ":" + '"' + str(round(m[1],3)) + '"'
+            hum = '"' + "humid(%)" + '"' + ":" + '"' + str(round(m[2],3)) + '"'
+            co2 = '"' + "co2(ppm)" + '"' + ":" + '"' + str(round(m[0],3)) + '"'
+            mylist = [tim,temp,hum,co2]
+            mystr = '{' + ','.join(map(str,mylist))+'}'
+            print(mystr)
+            mqtt_client.publish("{}/{}".format("/demo",'car_count'), mystr)
+            time.sleep(2)
     else:
         time.sleep(0.2)
 mqtt_client.disconnect()
